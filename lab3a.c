3a5cc6cee0d2f820eb79d258457ccced31dc11b9
Anbo Wei <anbo_wei@outlook.com>
2016-05-17 17:10:36 -0700

    Added unsigned to data types in read parsing, finished readSuperBlock code

--- lab3a.c
+++ lab3a.c
@@ -11,13 +11,16 @@ int blockCount;
 int blockSize;
 int fragmentSize;
 int blocksPerGroup; 
+int inodesPerGroup;
+int fragmentsPerGroup;
+int firstDataBlock;
 
 //Since the file format is in a little endian format, it is useful to turn those bytes into 
 //a big endian format (expected by the csv) procedurally. This function does this.
-ssize_t preadLittleEndian(int fd, char* buffer, size_t count, off_t offset) {
+ssize_t preadLittleEndian(int fd, unsigned char* buffer, size_t count, off_t offset) {
 	int readCount = pread(fd, buffer, count, offset);
 	for (int i = 0; i < readCount / 2; i++) {
-		char temp = buffer[i];
+		unsigned char temp = buffer[i];
 		buffer[i] = buffer[readCount - i - 1];
 		buffer[readCount - i - 1] = temp;
 	}
@@ -25,8 +28,8 @@ ssize_t preadLittleEndian(int fd, char* buffer, size_t count, off_t offset) {
 }
 
 //Returns the corresponding integer from a big-endian buffer containing the raw bytes
-int getIntFromBuffer(char* buffer, size_t count) {
-	int returnInt = 0;
+int getIntFromBuffer(unsigned char* buffer, size_t count) {
+	unsigned int returnInt = 0;
 	for (int i = 0; i < count; i++) {
 		returnInt = (returnInt << 8) + buffer[i];
 	}
@@ -46,13 +49,13 @@ void readSuperBlock(int fd) {
 	FILE* writeFileStream = fopen("super.csv", "w+");
 
 	//Read magic number
-	char* buffer = malloc(64); 
+	unsigned char* buffer = malloc(64); 
 	preadLittleEndian(fd, buffer, 2, 1080); //1080 is where the magic number resides
 		//no error checking
 	buffer[2] = '\0';
 
-	fprintf(writeFileStream, "%02X%02X%02X%02X,", 
-		buffer[0], buffer[1], buffer[2], buffer[3]);
+	fprintf(writeFileStream, "%02x%02x,", 
+		buffer[0], buffer[1]);
 
 	//Read and store total number of inodes
 	preadLittleEndian(fd, buffer, 4, 1024); //Offset for inode count
@@ -79,11 +82,26 @@ void readSuperBlock(int fd) {
 	fprintf(writeFileStream, "%d,", fragmentSize);
 
 	//Read and store blocks per group
-	preadLittleEndian(fd, buffer, 4, 1056); //Offset for block count
+	preadLittleEndian(fd, buffer, 4, 1056); //Offset for blocks per group
 	blocksPerGroup = getIntFromBuffer(buffer, 4);
 	fprintf(writeFileStream, "%d,", blocksPerGroup);
 
+	//Read and store inodes per group
+	preadLittleEndian(fd, buffer, 4, 1064); //Offset for inodes per group
+	inodesPerGroup = getIntFromBuffer(buffer, 4);
+	fprintf(writeFileStream, "%d,", inodesPerGroup);
 
+	//Read and store fragments per group
+	preadLittleEndian(fd, buffer, 4, 1060); //Offset for fragments per group
+	fragmentsPerGroup = getIntFromBuffer(buffer, 4);
+	fprintf(writeFileStream, "%d,", fragmentsPerGroup);
+
+	//Read and store first data block, as well as newline
+	preadLittleEndian(fd, buffer, 4, 1044); //Offset for fist data block
+	firstDataBlock = getIntFromBuffer(buffer, 4);
+	fprintf(writeFileStream, "%d\n", firstDataBlock);
+
+	fflush(writeFileStream);
 }
 
 
@@ -100,9 +118,11 @@ int main (int argc, const char* argv[]) {
 		exit(1);
 	}
 
-	char* buf = malloc(4);
-	preadLittleEndian(diskImageFD, buf, 4, 1056 );
+	unsigned char* buf = malloc(4);
+	preadLittleEndian(diskImageFD, buf, 4, 1064);
 	printf("%02X:%02X:%02X:%02X\n", buf[0], buf[1], buf[2], buf[3]);
 
 	printf("%d\n", getIntFromBuffer(buf, 4));
+
+	readSuperBlock(diskImageFD);
 }
